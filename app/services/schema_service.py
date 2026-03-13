db9caa01dc7056682f2fb2febef6569abd8a73e9
Jon Kent <29626876+Jon-Kent@users.noreply.github.com>
2026-03-13 16:41:39 +0000

    awesome refactor

--- app/services/schema_service.py
+++ app/services/schema_service.py
@@ -11,15 +11,5 @@ class ExistingSchema(Protocol):
 class SchemaService:
 
     def find_new_schemas(self, existing_schemas: list[ExistingSchema], schemas: list[Schema]) -> list[Schema]:
-        new_schemas: list[Schema] = []
-        for schema in schemas:
-            is_new = True
-            for existing in existing_schemas:
-                if schema.survey_id == existing.survey_id:
-                   if schema.schema_version == existing.schema_version:
-                       is_new = False
-
-            if is_new:
-                new_schemas.append(schema)
-
-        return new_schemas
+        existing_set = {(x.survey_id, x.schema_version) for x in existing_schemas}
+        return [s for s in schemas if (s.survey_id, s.schema_version) not in existing_set]
