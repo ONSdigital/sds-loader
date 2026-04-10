6971a26a7f77c2acc748723d3af0d8c609a681e2
Angus <Angus.goody@ons.gov.uk>
2026-04-10 13:29:07 +0100

    adding batching to write

--- app/repositories/dataset_storage/firestore_dataset_storage_repository.py
+++ app/repositories/dataset_storage/firestore_dataset_storage_repository.py
@@ -13,6 +13,7 @@ logger = get_logger()
 class FirestoreSettings(Protocol):
     project_id: str
     firestore_database: str
+    should_batch: bool
 
 
 class FirestoreDatasetStorageRepository(DatasetStorageRepositoryInterface):
@@ -33,7 +34,9 @@ class FirestoreDatasetStorageRepository(DatasetStorageRepositoryInterface):
             database=self.settings.firestore_database
         )
 
+        # Max size in bytes we can upload in one batch
         self.MAX_BATCH_SIZE_BYTES = 9 * 1024 * 1024
+        self.MAX_NUMBER_OF_WRITES_PER_BATCH = 500
 
         # Initialize Firestore collections
         self.dataset_collection = self.client.collection("datasets")
@@ -93,17 +96,108 @@ class FirestoreDatasetStorageRepository(DatasetStorageRepositoryInterface):
         unit_data_identifiers: list[str]
     ):
 
+        # Write to firebase in batches or not depends on the settings
+
+        if self.settings.should_batch:
+            self._store_dataset_with_batching(
+                dataset_id=dataset_id,
+                dataset_metadata=dataset_metadata,
+                unit_data_collection_with_metadata=unit_data_collection_with_metadata,
+                unit_data_identifiers=unit_data_identifiers
+            )
+        else:
+            self._store_dataset_without_batching(
+                dataset_id=dataset_id,
+                dataset_metadata=dataset_metadata,
+                unit_data_collection_with_metadata=unit_data_collection_with_metadata,
+                unit_data_identifiers=unit_data_identifiers
+            )
+
+    def _store_dataset_without_batching(
+        self,
+        dataset_id: Guid,
+        dataset_metadata: DatasetMetadataWithoutId,
+        unit_data_collection_with_metadata: list[UnitDataset],
+        unit_data_identifiers: list[str]
+    ):
+        """
+        Write the data to firestore without batching
+        """
         # Create a new document for this dataset
         new_dataset_document = self.dataset_collection.document(dataset_id)
+
+        # Store the core data first
+        new_dataset_document.set(dataset_metadata.model_dump(), merge=True)
 
         # Create a new collection for the units
         units_collection = new_dataset_document.collection("units")
 
         # Go through unit data
         for (unit_data, unit_identifier) in zip(unit_data_collection_with_metadata, unit_data_identifiers):
-
             # Create and save the unit data as a new sub document
             units_collection.document(unit_identifier).set(unit_data.model_dump())
+
+    def _store_dataset_with_batching(
+        self,
+        dataset_id: Guid,
+        dataset_metadata: DatasetMetadataWithoutId,
+        unit_data_collection_with_metadata: list[UnitDataset],
+        unit_data_identifiers: list[str]
+    ):
+        """
+        Use batches to write the data to firestore
+        """
+
+        # Create a new document for this dataset
+        new_dataset_document = self.dataset_collection.document(dataset_id)
+
+        # Store the core data first
+        new_dataset_document.set(dataset_metadata.model_dump(), merge=True)
+
+        # Create a new collection for the units
+        units_collection = new_dataset_document.collection("units")
+
+        # Initialise a batch
+        batch = self.client.batch()
+        batch_size_bytes = 0
+        batch_num_records = 0
+
+        # Go through unit data
+        for (unit_data, unit_identifier) in zip(unit_data_collection_with_metadata, unit_data_identifiers):
+            """
+            Add this unit to the current batch if ...
+
+            1. adding it does not exceed the batch size limits
+            2. adding it does not exceed the batch record limits
+            """
+
+            # Work out the size of this unit
+            unit_size = len(unit_data.model_dump().encode('utf-8'))
+
+            # Work out if the current batch is too big already
+            if (batch_size_bytes + unit_size >= self.MAX_BATCH_SIZE_BYTES) or (batch_num_records + 1 >= self.MAX_NUMBER_OF_WRITES_PER_BATCH):
+                # Commit the current batch
+                batch.commit()
+
+                # Start a new batch
+                batch = self.client.batch()
+                batch_size_bytes = 0
+                batch_num_records = 0
+
+            # Add the unit to the new batch
+            new_unit = units_collection.document(unit_identifier)
+            batch.set(new_unit, unit_data.model_dump(), merge=True)
+            batch_size_bytes += unit_size
+            batch_num_records += 1
+
+        # If we never exceeded batch limit we still need to commit
+        if batch_size_bytes > 0:
+            batch.commit()
+
+
+
+
+
 
     def delete_dataset_version(
         self,
