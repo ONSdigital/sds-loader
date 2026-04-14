26f633e6d06e34eb6fd625979e00bbb4eeb934cb
Angus <Angus.goody@ons.gov.uk>
2026-04-14 12:00:30 +0100

    bump

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,26 +1,9 @@
-from lagom import Singleton, dependency_definition
+from lagom import Singleton
 from lagom.container import Container
-from sds_common.publishers.gcs_schema_publisher import GcsSchemaPublisher
-from sds_common.publishers.github_schema_publisher import GithubSchemaPublisher
-from sdx_base.services.storage import StorageService
 
 from app import get_logger
-from app.broadcasters.fake_broadcaster import FakeBroadcaster
-from app.broadcasters.pubsub_broadcaster import PubsubBroadcaster
-from app.interfaces.dataset_broadcast_interface import DatasetBroadcastInterface
-from app.interfaces.dataset_deletion_repository_interface import DatasetDeletionRepositoryInterface
-from app.interfaces.dataset_source_repository_interface import DatasetSourceRepositoryInterface
-from app.interfaces.dataset_storage_repository_interface import DatasetStorageRepositoryInterface
 from app.profiles import PROFILES
-from app.repositories.dataset_deletion.fake_dataset_deletion_repository import FakeDatasetDeletionRepository
-from app.repositories.dataset_deletion.firestore_dataset_deletion_repository import FirestoreDatasetDeletionRepository
-from app.repositories.dataset_source.bucket_dataset_source_repository import BucketDatasetSourceRepository
-from app.repositories.dataset_source.fake_dataset_source_repository import FakeDatasetSourceRepository
-from app.repositories.dataset_storage.fake_dataset_storage_repository import FakeDatasetStorageRepository
-from app.repositories.dataset_storage.firestore_dataset_storage_repository import FirestoreDatasetStorageRepository
 from app.services.dataset_service import DatasetService, DatasetSettings
-from app.services.schema_service import SchemaService
-
 from app.settings import Settings, get_instance, QuickSettings
 
 logger = get_logger()
