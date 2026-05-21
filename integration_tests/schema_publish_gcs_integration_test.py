60f374b5f2ab0f068231b2272c860a9e1f1fa3b2
Angus <Angus.goody@ons.gov.uk>
2026-05-21 17:56:11 +0100

    bump

--- integration_tests/schema_publish_gcs_integration_test.py
+++ integration_tests/schema_publish_gcs_integration_test.py
@@ -56,7 +56,7 @@ class SchemaPublishGcsIntegrationTest(TestCase):
 
         """
         # TODO move this into folder?
-        self.bucket_service.upload_file_to_bucket("test_schema_success.json")
+        self.bucket_service.upload_file_to_bucket("integration_tests/test_data/test_schema_success.json")
 
         messages = poll_subscription(
             self.schema_success_pubsub_helper, test_schema_subscriber_id_success
