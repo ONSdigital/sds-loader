ead628b3360bd7943fb24d710d758c02f4bdbfab
jamesbryer <james.bryer@ons.gov.uk>
2026-05-21 09:14:26 +0100

    Update tests with new common stuff

--- integration_tests/dataset_publish_integration_test.py
+++ integration_tests/dataset_publish_integration_test.py
@@ -2,11 +2,14 @@ import unittest
 from sds_common.repositories.bucket_loader import BucketLoader
 from sds_common.services.bucket_service import BucketService
 from sds_common.services.http_service import HttpService
+from sds_common.services.sds_dataset_request_service import SdsDatasetRequestService
 from sds_common.test_helpers.integration_helpers import cleanup, inject_wait_time
 from sds_common.enums.buckets import Bucket
 from sds_common.config.config import CONFIG
 from unittest import TestCase
 from google.cloud import storage
+from sds_common.test_helpers.pub_sub_helper import PubSubHelper
+
 from app.settings import Settings
 
 
@@ -14,10 +17,13 @@ class DatasetPublishIntegrationTest(unittest.TestCase):
     @classmethod
     def setup_class(cls):
         cleanup()
+        cls.schema_queue_pubsub_helper = PubSubHelper(
+            CONFIG.PUBLISH_DATASET_TOPIC_ID
+        )
         cls.bucket_service = BucketService(
             Bucket.DATASET_BUCKET, BucketLoader()
         )
-        cls.http_service = HttpService.create(True)
+        cls.sds_dataset_request_service = SdsDatasetRequestService()
         cls.dataset_create_endpoint = f"{CONFIG.SDS_URL}{CONFIG.DATASET_CREATE_PATH}"
 
     @classmethod
@@ -37,7 +43,7 @@ class DatasetPublishIntegrationTest(unittest.TestCase):
 
         self.bucket_service.upload_file_to_bucket("integration_tests/test_data/dummy_dataset_40_encrypted.json")
 
-        response = self.http_service.make_get_request(self.dataset_create_endpoint)
+        response = self.sds_dataset_request_service.get_dataset_create()
 
         self.assertEqual(200, response.status_code)
 
@@ -47,6 +53,7 @@ class DatasetPublishIntegrationTest(unittest.TestCase):
                 "Expected dataset file to be deleted from the bucket when autodelete is enabled.",
             )
 
-        response = self.http_service.make_get_request(f"{CONFIG.SDS_URL}/v1/dataset_metadata?survey_id=test_survey_id_dataset_create_test&period_id=test_period_id")
-
-        self.assertEqual(len(response.json()), 1, "Expected one dataset metadata entry in the response.")
+        survey_id = "test_survey_id_dataset_create_test"
+        period_id = "test_period_id"
+        response = self.sds_dataset_request_service.get_dataset_metadata(survey_id, period_id)
+        self.assertEqual(response, 1, "Expected one dataset metadata entry in the response.")
