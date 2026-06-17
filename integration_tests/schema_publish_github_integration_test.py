584cc718afc47770c70e0d4fec149fa0f4625475
Angus <Angus.goody@ons.gov.uk>
2026-06-17 11:34:20 +0100

    bump

--- integration_tests/schema_publish_github_integration_test.py
+++ integration_tests/schema_publish_github_integration_test.py
@@ -29,7 +29,7 @@ class SchemaPublishIntegrationTest(TestCase):
         pubsub_setup(
             cls.schema_success_pubsub_helper, test_schema_subscriber_id_success
         )
-        inject_wait_time(3)  # Inject wait time to allow resources properly set up
+        inject_wait_time(5)  # Inject wait time to allow resources properly set up
 
     @classmethod
     def teardown_class(cls) -> None:
